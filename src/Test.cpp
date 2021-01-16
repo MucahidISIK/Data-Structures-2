df786c8d3956215b3f50728ca768d74ed9074c54
Mücahid IŞIK <56644702+MucahidISIK@users.noreply.github.com>
2021-01-16 19:24:11 +0300

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,11 +1,4 @@
-/**
-* @file b181210111_1A_Odev1
-* @description Kovaları ve kovaların içinde oluşturulan topların kontrolünün yapılması.
-* @course 1. Öğretim A Grubu
-* @assignment 1. Ödev
-* @date 03.11.2019
-* @author Mücahid IŞIK mucahid.isik@ogr.sakarya.edu.tr , Hüseyin HIRA huseyin.hira@ogr.sakarya.edu.tr 
-*/
+
 #include "pch.h"
 #include "Kova.h"
 #include "KovaKontrol.h"
